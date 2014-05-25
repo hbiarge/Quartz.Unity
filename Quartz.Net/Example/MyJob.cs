861d6c9bccfebcd929d96221eb30e6cc1e3d7446
Hugo Biarge <hugo@arosbi.com>
2014-05-25 10:32:33 +0200

    Example: Add missing file

---
+++ Quartz.Net/Example/MyJob.cs
@@ -0,0 +1,27 @@
+﻿namespace Example
+{
+    using System;
+
+    using Quartz;
+
+    public class MyJob : IJob
+    {
+        private readonly IConfigManager configManager;
+
+        public MyJob(IConfigManager configManager)
+        {
+            if (configManager == null)
+            {
+                throw new ArgumentNullException("configManager");
+            }
+
+            this.configManager = configManager;
+        }
+
+        public void Execute(IJobExecutionContext context)
+        {
+            Console.Write("Job fired! ");
+            Console.WriteLine("ConfigManager type is {0}", configManager.GetValue("type"));
+        }
+    }
+}
